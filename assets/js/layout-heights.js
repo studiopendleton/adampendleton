8989fe6250aa554ebb03adeffdcb828ad9b6409c
Alec Mapes-Frances <alecmapesfrances@gmail.com>
2024-11-10 11:18:18 -0500

    Delete unused scripts

--- assets/js/layout-heights.js
+++
@@ -1,9 +0,0 @@
-function layoutHeights() {
-    const header = document.querySelector('header');
-    const headerHeight = header.scrollHeight;
-
-    document.documentElement.style.setProperty('--header-height', `calc(${headerHeight}px + var(--margin))`);
-}
-
-layoutHeights();
-window.addEventListener('resize', layoutHeights);
--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -10,8 +10,5 @@
 
 <?php $isMobile = is_numeric(strpos(strtolower($_SERVER["HTTP_USER_AGENT"]), "mobile")); ?>
 
-  <?= js(['assets/js/global.js', '@auto']) ?>
-  <?php e($site->parallax()->toBool(), js(['assets/js/parallax.js'])) ?>
-
 </body>
 </html>
